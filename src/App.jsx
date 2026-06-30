f6518a61d35e8113d8fe2e5933715f61247d9c56
sagaringle09 <sagarbingle09@gmail.com>
2026-06-30 12:46:41 +0530

    feat: initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to Vite</h1>
+        <p>This is a simple Vite app.</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
